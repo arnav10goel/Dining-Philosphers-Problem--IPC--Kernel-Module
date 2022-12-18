4a4cfd9d83ec30f76ff16f24383a7a9597b6681e
Arnav_2021519 <arnav21519@iiitd.ac.in>
2022-12-18 22:06:33 +0530

    Q1 done? sockets needs to be done not correct code here

--- 2021519_Q2/Sockets/socket_receiver.c
+++ 2021519_Q2/Sockets/socket_receiver.c
@@ -6,22 +6,39 @@
 #include<sys/un.h>
 #include<string.h>
 
+#define MAX_IDS 50
+#define STR_LEN 6
+
 int main()
 {
-    int sockfd;
-    struct sockaddr_un server_addr;
+    char listofstrings[MAX_IDS][STR_LEN+1];
+    char listofalphabets[26][2] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
+                                "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
+                                "w", "x", "y", "z"};
+    
+    for(int i = 0; i < MAX_IDS; i++){
+        for(int j = 0; j < STR_LEN; j++){
+            listofstrings[i][j] = listofalphabets[rand()%26][0];
+        }
+        listofstrings[i][STR_LEN] = '\0';    
+    }
+
+    unsigned int sockfd;
+    struct sockaddr_un string_sender;
     char buffer[1024];
     int len;
 
     sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
-    if(sockfd < 0)
+    if(sockfd == -1)
     {
-        perror("socket");
-        exit(1);
+        perror("Socket Creation Failed");
+        exit(0);
     }
+    string_sender.sun_family = AF_UNIX;
+    char* socket_path = "sender_socket";
+    strcpy(string_sender.sun_path, socket_path);
+    unlink(string_sender.sun_path); //Unlink to avoid errors
 
-    server_addr.sun_family = AF_UNIX;
-    strcpy(server_addr.sun_path, "server_socket");
     if(connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
     {
         perror("connect");
