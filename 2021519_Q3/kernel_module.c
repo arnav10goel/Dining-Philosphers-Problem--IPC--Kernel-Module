964b3dfcf008a91a8c39dbe3def6ca23bb19ba7b
Arnav_2021519 <arnav21519@iiitd.ac.in>
2022-12-19 15:55:39 +0530

    Made changes to Q3

--- 2021519_Q3/kernel_module.c
+++ 2021519_Q3/kernel_module.c
@@ -1,9 +1,9 @@
 #include <linux/module.h>     
 #include <linux/kernel.h>     
 #include <linux/init.h>
-#include <linux/sched.h>   // Needed for task_struct
-#include <linux/stat.h>    // Needed for the macros
-#include <linux/moduleparam.h> // Needed for module_param
+#include <linux/sched.h>   
+#include <linux/stat.h>   
+#include <linux/moduleparam.h> 
 #include <linux/slab.h>
 #include <linux/uaccess.h>
 #include <linux/unistd.h>
@@ -13,29 +13,35 @@
 #include <linux/dcache.h>
 
 
-MODULE_LICENSE("GPL");        // The license  under which the module is distributed.
-MODULE_AUTHOR("Arnav Goel");// The author of the module.
+MODULE_LICENSE("GPL");       
+MODULE_AUTHOR("Arnav Goel");
 MODULE_DESCRIPTION("Linux Process Task Struct Reading Module"); // The Description of the module.
 
-int pid_input = 0; // Variable to store Process ID
+int pid_input = 0; // Variable to store Process ID as a command line input from the user
 
+//This is used to take input from the user in the form of a command line argument
 module_param(pid_input, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+MODULE_PARM_DESC(pid_input, "Process ID of the process whose information is to be printed");
+
 // Function to print the process information
 void print_process_info(void)
 {
     pid_t pid = (pid_t)pid_input;
+
     // Get the task_struct of the process
     struct task_struct *task = pid_task(find_vpid(pid), PIDTYPE_PID);
-    if (!task) {
-        printk(KERN_INFO "Invalid process ID: %d\n", pid);
+
+    //Error handling
+    if (task == -1) {
+        printk(KERN_INFO "Process ID not found: %d\n", pid);
         return;
     }
 
     // Print the pid, ppid, gpid, and uid of the process
-    printk(KERN_INFO "pid: %d\n", task->pid);
-    printk(KERN_INFO "ppid: %d\n", task->real_parent->pid);
-    printk(KERN_INFO "gpid: %d\n", task->group_leader->pid);
-    printk(KERN_INFO "uid: %d\n", task->cred->uid);
+    printk(KERN_INFO "PID of the given process is: %d\n", task->pid);
+    printk(KERN_INFO "PPID of the given process is: %d\n", task->real_parent->pid);
+    printk(KERN_INFO "GPID of the given process is: %d\n", task->group_leader->pid);
+    printk(KERN_INFO "UID of the given process is: %d\n", task->cred->uid);
 
     // Get the path of the process
     char* path = NULL;
@@ -45,28 +51,20 @@ void print_process_info(void)
         return;
     }
 
-    //Read the symbolic link to the process's executable
+    // Get the path of the process
     int len = 0;
     len = sprintf(path, "/proc/%d/exe", task->pid);
-    //len = vfs_readlink(path, path, len);
-    // if (len == -1) {
-    //     printk(KERN_INFO "Error reading link to process executable\n");
-    //     kfree(path);
-    //     return;
-    // }
-
     path[len] = '\0';
 
     //Print the path of the process
     printk(KERN_INFO "path: %s\n", path);
 
-    //Free the allocated memory
+    //Free the allocated memory for the path
     kfree(path);
 }       
 
 
-// This function defines what happens when this module is inserted into the kernel.
-// ie. when you run insmod command.
+//When you run insmod command, this function is called.
 static int __init hello_init(void)
 {
     printk(KERN_INFO "Hello world! Entering the Module.\n");
@@ -74,8 +72,7 @@ static int __init hello_init(void)
     return 0;    // Non-zero return means that the module couldn't be loaded.
 }
 
-// This function defines what happens when this module is removed from the kernel.
-// ie.when you run rmmod command.
+// When you run rmmod command, this function is called.
 static void __exit hello_cleanup(void)
 {
     printk(KERN_INFO "Cleaning up module.\n");
