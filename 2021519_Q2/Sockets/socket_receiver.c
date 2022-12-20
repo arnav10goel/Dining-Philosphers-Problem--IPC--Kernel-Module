79611a4787086c73709ebb0e5f97e68b1f44a49b
Arnav_2021519 <arnav21519@iiitd.ac.in>
2022-12-20 12:43:32 +0530

    Chal gaye sockets

--- 2021519_Q2/Sockets/socket_receiver.c
+++ 2021519_Q2/Sockets/socket_receiver.c
@@ -17,6 +17,8 @@ int main()
     memset(&string_receiver, 0, sizeof(string_receiver));
     int len, server_socket;
 
+    unlink(socket_path);
+
     sockfd_recv = socket(AF_UNIX, SOCK_STREAM, 0);
     if(sockfd_recv == -1)
     {
@@ -26,31 +28,55 @@ int main()
     string_receiver.sun_family = AF_UNIX;
     
     strcpy(string_receiver.sun_path, socket_path);
-    // unlink(string_receiver.sun_path);
 
-    if(connect(sockfd_recv, (struct sockaddr *)&string_receiver, sizeof(string_receiver)) == -1)
+    if(bind(sockfd_recv, (const struct sockaddr *)&string_receiver, sizeof(struct sockaddr_un)) == -1)
     {
-        perror("Connection Error");
+        perror("Binding Error");
+    }
+    printf("Socket Path: %s\n", string_receiver.sun_path);
+    printf("Bind Call Successfull\n");
+
+    int ret = listen(sockfd_recv, 20);
+    if(ret == -1)
+    {
+        perror("Listening Error");
+        exit(1);
     }
 
-    int id_received = 0;
+    int id_received = 1;
     int highest_id = 0; 
     char string_received[STR_LEN+1];
 
     while(1){
-        
+        printf("Waiting on accept() sys call\n");
+        server_socket = accept(sockfd_recv, (struct sockaddr *)&string_receiver, (socklen_t*)&len);
+        if(server_socket == -1)
+        {
+            perror("Accept Error");
+            exit(1);
+        }
+        printf("Accept Call Successfull\n");
+
         while(1){
-            if(read(sockfd_recv, string_received, STR_LEN+1) == -1)
+            if(read(server_socket, string_received, sizeof(string_received)) == -1)
             {
                 perror("Reading Error");
                 exit(1);
             }
             printf("ID Received: %d String Received: %s\n", id_received, string_received);
             id_received++;
+            memset(string_received, 0, sizeof(string_received));
+            if(id_received > MAX_IDS){
+                break;
+            }
+
         }
-    
+        close(server_socket);
+        break;
     }
 
     close(sockfd_recv);
-    return 0;
+    printf("Exiting Server");
+    unlink(socket_path);
+    exit(0);
 }
--- 2021519_Q2/Sockets/socket_sender.c
+++ 2021519_Q2/Sockets/socket_sender.c
@@ -13,22 +13,26 @@
 int main()
 {
     //Code snippet to generate 50 random strings of length 5
-    char listofstrings[MAX_IDS][STR_LEN+2];
+    char listofstrings[MAX_IDS][STR_LEN+1];
     char listofalphabets[26][2] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
                                 "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
                                 "w", "x", "y", "z"};
     
-    for(int i = 0; i < 50; i++){
+    for(int i = 0; i < MAX_IDS; i++){
         for(int j = 0; j < STR_LEN; j++){
             listofstrings[i][j] = listofalphabets[rand()%26][0];
         }
-        listofstrings[i][STR_LEN] = i+1;
-        listofstrings[i][STR_LEN+1] = '\0';    
+        listofstrings[i][STR_LEN] = '\0';    
+    }
+
+
+    for(int i = 0; i < 50; i++){
+        printf("String : %s\n", listofstrings[i]);
     }
 
     unsigned int sockfd_sender;
     struct sockaddr_un string_sender;
-    int len, server_socket;
+    int len;
 
     sockfd_sender = socket(AF_UNIX, SOCK_STREAM, 0);
     if(sockfd_sender < 0)
@@ -37,6 +41,8 @@ int main()
         exit(1);
     }
 
+    memset(&string_sender, 0, sizeof(string_sender));
+
     string_sender.sun_family = AF_UNIX;
 
     printf("Socket Path: %s\n", socket_path);
@@ -44,46 +50,34 @@ int main()
     printf("Socket Path: %s\n", string_sender.sun_path);
 
     len = strlen(string_sender.sun_path)+1 + sizeof(string_sender.sun_family);
-    unlink(socket_path);
-    if(bind(sockfd_sender, (struct sockaddr*)&string_sender, len) == -1)
+
+    if(connect(sockfd_sender, (struct sockaddr*)&string_sender, sizeof(struct sockaddr_un)) == -1)
     {
         perror("Socket Connection Error");
         exit(1);
     }
 
-    if(listen(sockfd_sender, 20) == -1)
-    {
-        perror("Listening Error");
-        exit(1);
-    }
-
     int id_sent = 0;
+    int id_tracker = 0;
     int id_received = 0;
     while(1){
-        // printf("HII");
-        server_socket = accept(sockfd_sender, NULL, NULL);
-        if(server_socket == -1)
-        {
-            perror("Accepting Error");
-            exit(1);
-        }
         for(int j = 0; j < 5; j++){
-            if(write(sockfd_sender, listofstrings[id_sent], strlen(listofstrings[id_sent])+1) == -1){
+            if(write(sockfd_sender, listofstrings[id_sent], sizeof(listofstrings[id_sent])) == -1){
                 perror("write");
                 exit(0);
             }
+            printf("ID: %d String written is %s\n", id_sent, listofstrings[id_sent]);
             id_sent += 1;
-            if(id_received == 5+id_sent){
+            if(id_sent == 5+id_tracker){
                 id_received = id_sent;
+                id_tracker += 5;
                 printf("Highest ID received: %d\n", id_received);
             }
-            if(id_sent == 50){
-                break;
-            }
         }
-        close(server_socket);
+        if(id_sent == 50){
+            break;
+        }
     }
-
     close(sockfd_sender);
     exit(0);
 }
